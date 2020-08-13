77eb5cb67b42d041fb3ec743b0df84e0692ed904
Eevee8856 <datrix5678@gmail.com>
2020-08-13 14:18:35 +0000

    Initial commit

---
+++ server/src/api/utils/utils.js
@@ -0,0 +1,37 @@
+const axios = require('axios').default;
+
+const renameKey = (obj, old_key, new_key) => {
+  if (old_key !== new_key) {
+    Object.defineProperty(obj, new_key,
+      Object.getOwnPropertyDescriptor(obj, old_key));
+    delete obj[old_key];
+  }
+};
+
+const requests = (url) =>{
+  const options ={
+    method: 'GET',
+    responseType: 'json'
+  }
+  return new Promise((resolve , reject) =>{
+    axios.get(url , {options})
+      .then(res =>{
+        const data = res.data;
+        return data;
+      }).then(json =>{
+        resolve(json)
+      })
+  })
+}
+
+const currencyFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2
+});
+
+module.exports = {
+  renameKey,
+  requests,
+  currencyFormatter
+};
